9e3560489915b151c422adf08e7ebd3bdd1472cd
Birra Laxman <birralaxman36@gmail.com>
2026-06-18 16:57:06 +0530

    Added password show hide feature

--- Frontend/assets/js/common.js
+++ Frontend/assets/js/common.js
@@ -146,3 +146,20 @@ function setRating(value){
     });
 
 }
+
+// ======================================
+// EYE PASSWORD
+// ======================================
+function togglePassword(){
+
+    const password = document.getElementById("password");
+    const eye = document.getElementById("eye");
+
+    if(password.type === "password"){
+        password.type = "text";
+        eye.innerHTML = "🙈";
+    }else{
+        password.type = "password";
+        eye.innerHTML = "👁️";
+    }
+}
